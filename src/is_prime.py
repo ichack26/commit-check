5f6c6536006daa0360db3f8b3cda101fcded23d8
Anson Chan <ahc325@ic.ac.uk>
2026-02-01 09:16:55 +0000

    reverse demo

--- src/is_prime.py
+++ src/is_prime.py
@@ -1,10 +1,12 @@
-import math
-
 def is_prime(n):
     if n <= 1:
         return False
-   
-    for i in range(2, n):
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    limit = int(n ** 0.5) + 1
+    for i in range(3, limit, 2):
         if n % i == 0:
             return False
    
